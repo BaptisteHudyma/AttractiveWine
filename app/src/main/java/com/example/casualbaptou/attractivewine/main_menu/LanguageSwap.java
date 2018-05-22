9cc11a9af7725e5e7d589cdc0e3862aa733dd65c
Alexis <knokerrr@gmail.com>
2018-05-22 22:53:38 +0200

    English and French string value + Class for swap language

---
+++ app/src/main/java/com/example/casualbaptou/attractivewine/main_menu/LanguageSwap.java
@@ -0,0 +1,24 @@
+package com.example.casualbaptou.attractivewine.main_menu;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import java.util.Locale;
+
+public class LanguageSwap {
+
+    public static void changeLanguage(Resources res, String locale) {
+        Configuration config;
+        config = new Configuration(res.getConfiguration());
+
+        switch (locale) {
+            case "fr":
+                config.setLocale(Locale.FRANCE);
+                break;
+            case "en":
+                config.setLocale(Locale.ENGLISH);
+                break;
+        }
+        res.updateConfiguration(config, res.getDisplayMetrics());
+    }
+}
